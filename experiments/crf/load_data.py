8215227fa3341d52f0accbfed69fb2079adf8e18
binhnq <quangbinh.nguyentrong@gmail.com>
2018-03-28 11:30:49 +0700

    up code

---
+++ experiments/crf/load_data.py
@@ -0,0 +1,29 @@
+from tools import readers
+
+
+PATH = '../../corpus/vlsp2016/{}.txt'
+
+
+def get_tokenizer(fileids=('train', 'dev', 'test')):
+    """
+
+    :return: train_set, dev_set, test_set
+    """
+    len_fields = 2
+    sep = '\t'
+
+    out_data = []
+
+    if 'train' in fileids:
+        train_set = [sen for sen in readers.iob_reader(PATH.format('train'), len_fields, sep=sep)]
+        out_data.append(train_set)
+
+    if 'dev' in fileids:
+        dev_set = [sen for sen in readers.iob_reader(PATH.format('dev'), len_fields, sep=sep)]
+        out_data.append(dev_set)
+
+    if 'test' in fileids:
+        test_set = [sen for sen in readers.iob_reader(PATH.format('test'), len_fields, sep=sep)]
+        out_data.append(test_set)
+
+    return out_data
